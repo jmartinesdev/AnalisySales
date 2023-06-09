51df1ea8c6886da6ed1a8700ed4246fe071e7dc0
Jakeline Martines <jakeline_13_martines@hotmail.com>
2023-06-09 12:21:35 +0000

    Group the Stores and Cards columns to discover the revenue for each store and then add the revenue for each product.

--- sales.py
+++ sales.py
@@ -14,12 +14,16 @@ total_earning = df['Final Value'].sum()
 print(f'The billing for the month of December is {total_earning}')
 
 """
-Analyze revenue by store
+Analyze revenue by store and grouping the store to check the billing per shop
 """
 
-billing_per_store = df[['Stores', 'Final Value']]
-print(billing_per_store)
+billing_by_store = df[['Stores', 'Final Value']].groupby("Stores").sum()
+print(f'This is the billing by each stores: {billing_by_store}')
 
 """
-grouping the store to check the billing per shop
+Group the Stores and Cards columns to discover the revenue for each store and then add the revenue for each product.
 """
+billing_by_card = df[['Stores', 'Cards', 'Final Value']].groupby(['Stores', 'Cards']).sum()
+print(billing_by_card)
+
+
